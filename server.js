bd4cc56ac9882fcabb15d8a7ef68e88300fa2ada
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-01 17:57:41 -0400

    Set up server bare bones and changed package.json to have all I need.

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+// server requirements
 const express = require("express");
 const fs = require("fs");
-
+// created express instance
 const app = express();
+// establishing PORT
+var PORT = process.env.PORT || 8080;
+// middleware to handle express data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// server listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
