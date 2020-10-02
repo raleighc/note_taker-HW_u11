e9eeb1622aeddccf8231990855d47c37b7d1850b
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-01 20:04:19 -0400

    added gets for html connections.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server requirements
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 // created express instance
 const app = express();
+const db = require("./db/db.json");
 // establishing PORT
 var PORT = process.env.PORT || 8080;
 // middleware to handle express data parsing
@@ -10,9 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+// HTML Routes
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
 
 // server listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
