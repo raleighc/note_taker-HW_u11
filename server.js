8c1f1856c82a3eb56124d6ba6c5bb059c5da5419
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-01 21:04:31 -0400

    added get for api array

--- server.js
+++ server.js
@@ -4,13 +4,31 @@ const path = require("path");
 const fs = require("fs");
 // created express instance
 const app = express();
-const db = require("./db/db.json");
+const dbArray = require("./db/db.json");
 // establishing PORT
 var PORT = process.env.PORT || 8080;
 // middleware to handle express data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// API Routes
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        error: true,
+        data: null,
+        message: "Failed retrieving notes.",
+      });
+    }
+    res.json({
+      error: false,
+      data: JSON.parse(data),
+      message: "Retrieved notes.",
+    });
+  });
+});
 
 // HTML Routes
 app.get("/notes", function (req, res) {
@@ -20,8 +38,6 @@ app.get("/notes", function (req, res) {
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
-
 
 // server listener
 app.listen(PORT, function () {
